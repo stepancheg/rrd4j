stepancheg/rrd4j

commit 0d04a1d7e212ba7ba56d9ebbd9fb363fdb38bd13

Correction of issue #61, patch from Stepan Koltsov applied.

src/main/java/org/rrd4j/core/jrrd/Archive.java
@@ -113,7 +113,7 @@ public class Archive {
             pointer = currentRow + chunk.start + 1;
         }
 
-        db.rrdFile.ras.seek(dataOffset + (chunk.dsCount * pointer * 8));
+        db.rrdFile.seek((dataOffset + (chunk.dsCount * pointer * 8)));
 
         double[][] data = chunk.data;
 
@@ -136,7 +136,7 @@ public class Archive {
                 if (pointer >= rowCount) {
                     pointer -= rowCount;
 
-                    db.rrdFile.ras.seek(dataOffset + (chunk.dsCount * pointer * 8));
+                    db.rrdFile.seek(dataOffset + (chunk.dsCount * pointer * 8));
                 }
 
                 for (int ii = 0; ii < chunk.dsCount; ii++) {
@@ -218,7 +218,7 @@ public class Archive {
             int counter = 0;
             int row = currentRow;
 
-            db.rrdFile.ras.seek(dataOffset + (row + 1) * db.header.dsCount * 8);
+            db.rrdFile.seek(dataOffset + (row + 1) * db.header.dsCount * 8);
 
             long lastUpdate = db.lastUpdate.getTime() / 1000;
             int pdpStep = db.header.pdpStep;
@@ -232,7 +232,7 @@ public class Archive {
                 if (row == rowCount) {
                     row = 0;
 
-                    db.rrdFile.ras.seek(dataOffset);
+                    db.rrdFile.seek(dataOffset);
                 }
 
                 long now = (lastUpdate - lastUpdate % (pdpCount * pdpStep))
@@ -285,12 +285,12 @@ public class Archive {
         }
         values = new double[db.header.dsCount][rowCount];
         int row = currentRow;
-        db.rrdFile.ras.seek(dataOffset + (row + 1) * db.header.dsCount * 8);
+        db.rrdFile.seek(dataOffset + (row + 1) * db.header.dsCount * 8);
         for (int counter = 0; counter < rowCount; counter++) {
             row++;
             if (row == rowCount) {
                 row = 0;
-                db.rrdFile.ras.seek(dataOffset);
+                db.rrdFile.seek(dataOffset);
             }
             for (int col = 0; col < db.header.dsCount; col++) {
                 double value = db.rrdFile.readDouble();

src/main/java/org/rrd4j/core/jrrd/RRDFile.java
@@ -1,10 +1,12 @@
 package org.rrd4j.core.jrrd;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  * This class is used read information from an RRD file. Writing
@@ -19,10 +21,10 @@ import java.nio.ByteOrder;
 class RRDFile implements Constants {
     private int alignment;
     private int longSize = 4;
-    final RandomAccessFile ras;
 
-    private ByteBuffer bbuffer = ByteBuffer.allocate(1024);
-    private byte[] buffer = bbuffer.array();
+    private final FileInputStream underlying;
+    private final MappedByteBuffer mappedByteBuffer;
+
     private ByteOrder order;
 
     RRDFile(String name) throws IOException {
@@ -30,20 +32,35 @@ class RRDFile implements Constants {
     }
 
     RRDFile(File file) throws IOException {
-        ras = new RandomAccessFile(file, "r");
-        initDataLayout(file);
-    }
+        long len = file.length();
+        if (len > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException(
+                    "RRDFile cannot read files larger than 2**31 because of limitations of java.nio.ByteBuffer");
+        }
 
-    private int read(int len) throws IOException {
-        bbuffer.clear();
-        int read = ras.read(buffer, 0, len);
-        return read;
+        boolean ok = false;
+        try {
+            underlying = new FileInputStream(file);
+            mappedByteBuffer = underlying.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, len);
+            initDataLayout(file);
+            ok = true;
+        } finally {
+            if (!ok) {
+                try {
+                    close();
+                } catch (Throwable ignored) {
+                }
+                // and then rethrow
+            }
+        }
     }
 
     private void initDataLayout(File file) throws IOException {
 
         if (file.exists()) {    // Load the data formats from the file
-            read(32);
+            byte[] buffer = new byte[32];
+            mappedByteBuffer.get(buffer);
+            ByteBuffer bbuffer = ByteBuffer.wrap(buffer);
 
             int index;
 
@@ -57,6 +74,7 @@ class RRDFile implements Constants {
             else {
                 throw new IOException("Invalid RRD file");
             }
+            mappedByteBuffer.order(order);
             bbuffer.order(order);
 
             switch (index) {
@@ -84,7 +102,8 @@ class RRDFile implements Constants {
         }
         else {                // Default to data formats for this hardware architecture
         }
-        ras.seek(0);    // Reset file pointer to start of file
+        // Reset file pointer to start of file
+        mappedByteBuffer.rewind();
     }
 
     private int indexOf(byte[] pattern, byte[] array) {
@@ -100,42 +119,39 @@ class RRDFile implements Constants {
     }
 
     double readDouble() throws IOException {
-        read(8);
-        return bbuffer.getDouble();
+        return mappedByteBuffer.getDouble();
     }
 
     int readInt() throws IOException {
-        read(4);
-        return bbuffer.getInt();
+        return mappedByteBuffer.getInt();
     }
 
     int readLong() throws IOException {
-        read(longSize);
         if(longSize == 4) {
-            return bbuffer.getInt();
+            return mappedByteBuffer.getInt();
         }
         else {
-            return (int) bbuffer.getLong();
+            return (int) mappedByteBuffer.getLong();
         }
     }
 
     String readString(int maxLength) throws IOException {
+        byte[] array = new byte[maxLength];
+        mappedByteBuffer.get(array);
 
-        ras.read(buffer, 0, maxLength);
-
-        return new String(buffer, 0, maxLength).trim();
+        return new String(array, 0, maxLength).trim();
     }
 
     void skipBytes(int n) throws IOException {
-        ras.skipBytes(n);
+        mappedByteBuffer.position(mappedByteBuffer.position() + n);
     }
 
     int align(int boundary) throws IOException {
 
-        int skip = (int) (boundary - (ras.getFilePointer() % boundary)) % boundary;
+        int skip = (int) (boundary - (mappedByteBuffer.position() % boundary)) % boundary;
 
         if (skip != 0) {
-            ras.skipBytes(skip);
+            mappedByteBuffer.position(mappedByteBuffer.position() + skip);
         }
 
         return skip;
@@ -146,19 +162,23 @@ class RRDFile implements Constants {
     }
 
     long info() throws IOException {
-        return ras.getFilePointer();
+        return mappedByteBuffer.position();
     }
 
     long getFilePointer() throws IOException {
-        return ras.getFilePointer();
+        return mappedByteBuffer.position();
     }
 
     void close() throws IOException {
-        ras.close();
+        if (underlying != null) {
+            underlying.close();
+        }
     }
 
     void read(ByteBuffer bb) throws IOException{
-        ras.getChannel().read(bb);
+        int count = bb.remaining();
+        bb.put((ByteBuffer) mappedByteBuffer.duplicate().limit(mappedByteBuffer.position() + count));
+        mappedByteBuffer.position(mappedByteBuffer.position() + count);
     }
 
     UnivalArray getUnivalArray(int size) throws IOException {
@@ -170,5 +190,9 @@ class RRDFile implements Constants {
      */
     int getBits() {
         return longSize * 8;
+    }
+
+    public void seek(long position) {
+        mappedByteBuffer.position((int) position);
     }
 }

src/test/java/org/rrd4j/core/jrrd/JrrdTest.java
@@ -96,7 +96,7 @@ public class JrrdTest {
             Assert.assertEquals("bad primary value", 1.43161853E7, rrd.getArchive(0).getCDPStatusBlock(0).primary_value, 1);
         }
         Assert.assertEquals("bad primary value", 1.4316557620000001E7, rrd.getArchive(1).getCDPStatusBlock(0).value, 1);
-        rrd.rrdFile.ras.seek( rrd.getArchive(0).dataOffset + 16 * rrd.getArchive(0).currentRow);
+        rrd.rrdFile.seek( rrd.getArchive(0).dataOffset + 16 * rrd.getArchive(0).currentRow);
         double speed = readDouble(rrd.rrdFile);
         double weight = readDouble(rrd.rrdFile);
         Assert.assertEquals(1.4316185300e+07, speed, 1e-7);
@@ -107,12 +107,7 @@ public class JrrdTest {
     }
     
    Double readDouble(RRDFile rrdFile) throws IOException {
-       RandomAccessFile ras = rrdFile.ras;
-       byte[] buffer = new byte[8];
-       ras.read(buffer);
-       ByteBuffer bb = ByteBuffer.wrap(buffer);
-       bb.order(rrdFile.isBigEndian() ? ByteOrder.BIG_ENDIAN: ByteOrder.LITTLE_ENDIAN);
-       return bb.getDouble();
+       return rrdFile.readDouble();
    }
 
 }

commit f201552095f1daefcb3985ed3bff6df5bd719cd9

Fix crash when requested time is out of range of archive
Bug was introduced in commit 0d04a1d7e212ba7ba56d9ebbd9fb363fdb38bd13.
Archive.loadData did seek beyond end of file, RRDFile was implemented
with RandomAccessFile which allows it. Now RRDFile is implemented
with ByteBuffer, which throws if requested too large position.

src/main/java/org/rrd4j/core/jrrd/Archive.java
@@ -127,7 +127,13 @@ public class Archive {
             rowIndexPointer = currentRow + chunk.startOffset + 1;
         }
 
-        db.rrdFile.seek((dataOffset + (chunk.dsCount * rowIndexPointer * Constants.SIZE_OF_DOUBLE)));
+        if (rowIndexPointer < rowCount) {
+            db.rrdFile.seek((dataOffset + (chunk.dsCount * rowIndexPointer * Constants.SIZE_OF_DOUBLE)));
+        } else {
+            // Safety net: prevent from reading random portions of file
+            // if something went wrong
+            db.rrdFile.seekToEndOfFile();
+        }
 
         double[][] data = chunk.data;
 

src/main/java/org/rrd4j/core/jrrd/RRDFile.java
@@ -195,4 +195,8 @@ class RRDFile implements Constants {
     public void seek(long position) {
         mappedByteBuffer.position((int) position);
     }
+
+    public void seekToEndOfFile() {
+        mappedByteBuffer.position(mappedByteBuffer.limit());
+    }
 }

src/test/java/org/rrd4j/core/jrrd/JrrdTest.java
@@ -102,6 +102,11 @@ public class JrrdTest {
         DataChunk data = rrd.getData(ConsolidationFunctionType.AVERAGE, 920802300, 920808900, 300);
         Assert.assertEquals(0.02, data.toPlottable("speed").getValue(920802300), 1e-7);
         Assert.assertEquals(1.0, data.toPlottable("weight").getValue(920802300), 1e-7);
+
+        {
+            // Just check it does not crash
+            rrd.getData(ConsolidationFunctionType.AVERAGE, Integer.MAX_VALUE - 100000, Integer.MAX_VALUE - 80000, 300);
+        }
     }
 
     Double readDouble(RRDFile rrdFile) throws IOException {